cd1d99c1a6887965a6de0b137e1f6deabb4b9ebf
Joao Victor Franco Rosa <joao.franco@totvs.com.br>
2020-12-08 21:34:19 -0300

    Configuracao do Projeto

---
+++ src/main/java/com/totvs/desafioAgro/DesafioAgroApplication.java
@@ -0,0 +1,13 @@
+package com.totvs.desafioAgro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAgroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totvs/desafioAgro/DesafioAgroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totvs.desafioAgro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAgroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
